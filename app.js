1a53b24213a583e4ef0a36cc93d21548bdfe5d1e
Habib Yeo <habibyeo2@gmail.com>
2020-07-16 23:52:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var ye = [38[67]];
+
+function na (arr){
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j])
+        }
+    }
+}
+
+function yeah (nine, ten){
+if(nine > ten);
+return overTen;
+}
+
+function areTheyEqual (first, second){
+    console.log(first);
+    console.log(second);
+    if(first == second){
+        return 'they are the same'
+    } else {
+        return 'they are NOT the same'
+    }
+}
+
+function cars (brand, model){
+    return brand + " " + model;
+};
+console.log(cars("toyota","supra"))
